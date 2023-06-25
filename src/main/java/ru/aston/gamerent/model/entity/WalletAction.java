ce2e8347c6d4fe0579fa2f63bd9b42d429c78fdb
Vladimir Shahnyuk <107857461+ValdemarSheesh@users.noreply.github.com>
2023-06-26 02:59:01 +0300

    fill WalletAction

--- src/main/java/ru/aston/gamerent/model/entity/WalletAction.java
+++ src/main/java/ru/aston/gamerent/model/entity/WalletAction.java
@@ -1,10 +1,63 @@
 package ru.aston.gamerent.model.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
+@Table(name = "wallet_actions")
 public class WalletAction {
+
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @Column(name = "transaction_value", nullable = false)
+    private Double transactionValue;
+
+    @Column(name = "transaction_time", nullable = false)
+    private LocalDateTime transactionTime;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "type_id")
+    private ActionType actionType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WalletAction that = (WalletAction) o;
+        return Objects.equals(wallet, that.wallet)
+                && Objects.equals(transactionValue, that.transactionValue)
+                && Objects.equals(transactionTime, that.transactionTime)
+                && Objects.equals(actionType, that.actionType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(wallet, transactionValue, transactionTime, actionType);
+    }
 }
