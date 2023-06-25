3a4605fda3532081f011889dffe49aeabade58bc
Masha Chachik <mashafadik@yahoo.com>
2023-06-25 23:13:25 +0300

    add Game

--- src/main/java/ru/aston/gamerent/model/entity/Game.java
+++ src/main/java/ru/aston/gamerent/model/entity/Game.java
@@ -1,10 +1,64 @@
 package ru.aston.gamerent.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
+@Table(name = "games")
 public class Game {
     @Id
-    Long id;
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id")
+    private Developer developer;
+
+    @Column(name = "release_date")
+    private Date releaseDate;
+
+    @Column
+    private String description;
+
+    @Column
+    private Double price;
+
+    @Column
+    private String image;
+
+    @Column(name = "trailer_url")
+    private String trailerUrl;
+
+    @Column(name = "create_time")
+    private LocalDateTime createTime;
+
+    @Column(name = "update_time")
+    private LocalDateTime updateTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+        return title == game.title;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+
 }
