4d825d95739b163d93ca43feba75de91be74a784
Aleksandr Mikhalevich <101008907+AleksandrMikhalevich@users.noreply.github.com>
2023-06-25 18:22:26 +0300

    [GAMERENT-1] User, Role, UsersRole entities added, Liquibase changelo… (#1)
    * [GAMERENT-1] User, Role, UsersRole entities added, Liquibase changelog for this entities added
    
    * [GAMERENT-1] Changes in User and Role entities

---
+++ src/main/java/ru/aston/gamerent/model/entity/RoleNameEnum.java
@@ -0,0 +1,6 @@
+package ru.aston.gamerent.model.entity;
+
+public enum RoleNameEnum {
+    ROLE_ADMIN,
+    ROLE_USER
+}
--- src/main/java/ru/aston/gamerent/model/entity/User.java
+++ src/main/java/ru/aston/gamerent/model/entity/User.java
@@ -1,10 +1,81 @@
 package ru.aston.gamerent.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
+@Table(name = "users")
 public class User {
+
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "registration_time")
+    private LocalDateTime registrationTime;
+
+    @Column(name = "update_time")
+    private LocalDateTime updateTime;
+
+    private String phone;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "is_blocked")
+    private Boolean isBlocked;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @ToString.Exclude
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
 }
