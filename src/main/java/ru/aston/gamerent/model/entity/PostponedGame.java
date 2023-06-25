a378bd3eef1a85dd6fae8d50c1cee6d981f5bc3c
Vladimir Shahnyuk <107857461+ValdemarSheesh@users.noreply.github.com>
2023-06-26 02:59:21 +0300

    fill PostponedGame

--- src/main/java/ru/aston/gamerent/model/entity/PostponedGame.java
+++ src/main/java/ru/aston/gamerent/model/entity/PostponedGame.java
@@ -1,10 +1,59 @@
 package ru.aston.gamerent.model.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
+@Table(name = "postponed_games")
 public class PostponedGame {
+
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @Column(name = "postponed_time", nullable = false)
+    private LocalDateTime postponedTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostponedGame that = (PostponedGame) o;
+        return user.equals(that.user)
+                && game.equals(that.game)
+                && postponedTime.equals(that.postponedTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, game, postponedTime);
+    }
 }
