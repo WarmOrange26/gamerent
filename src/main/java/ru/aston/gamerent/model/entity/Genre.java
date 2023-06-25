e1e9936ae47b9e25bde1e53fdf74fe7ce4c39774
Masha Chachik <mashafadik@yahoo.com>
2023-06-25 23:31:57 +0300

    add imports

--- src/main/java/ru/aston/gamerent/model/entity/Genre.java
+++ src/main/java/ru/aston/gamerent/model/entity/Genre.java
@@ -1,7 +1,16 @@
 package ru.aston.gamerent.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Objects;
 
