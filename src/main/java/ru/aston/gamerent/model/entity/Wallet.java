7cc3062ffcbcc0008e03c1e012e8062d87ee248c
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2023-06-26 15:21:34 +0500

    add Wallet

--- src/main/java/ru/aston/gamerent/model/entity/Wallet.java
+++ src/main/java/ru/aston/gamerent/model/entity/Wallet.java
@@ -1,10 +1,68 @@
 package ru.aston.gamerent.model.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ru.aston.gamerent.model.entity.enums.CurrencyCode;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
+@Table(name = "wallets")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Wallet {
+
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "currency")
+    @Enumerated(EnumType.STRING)
+    private CurrencyCode currency;
+
+    @Column(name = "value")
+    private BigDecimal value;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id")
+    private User users;
+
+    @OneToMany(mappedBy = "wallet", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<WalletAction> walletActions;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wallet wallet = (Wallet) o;
+        return Objects.equals(currency, wallet.currency) && Objects.equals(value, wallet.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, value);
+    }
 }
