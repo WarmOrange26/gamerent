4310696d1077a0a162ce5a047ac09423f09a390c
Dmitry Gityuk <82968604+DmitryGityuk@users.noreply.github.com>
2023-06-27 16:23:26 +0500

    DG PR (#6)
    * add Wallet
    
    * add Developer
    
    * add CurrencyCode
    
    * add ActionType
    
    * add changelog GAMERENT-DG-26-06-2023-changelog.yaml
    
    * add file include in changelog
    
    * changed equals and hashcode, fetchType.
    added column description.
    
    * changed equals and hashcode, fetchType, set-> list.
    added column description.
    
    * added email to author.
    changed fileName.
    
    * changed fileName.
    
    * changed access
    added getter
    
    ---------
    
    Co-authored-by: WarmOrange26 <137680029+WarmOrange26@users.noreply.github.com>

--- src/main/java/ru/aston/gamerent/model/entity/ActionType.java
+++ src/main/java/ru/aston/gamerent/model/entity/ActionType.java
@@ -1,10 +1,47 @@
 package ru.aston.gamerent.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
 
 @Entity
+@Table(name = "action_types")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ActionType {
+
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "action", nullable = false)
+    private String action;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ActionType actionType = (ActionType) o;
+        return action.equals(actionType.action);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(action);
+    }
 }
