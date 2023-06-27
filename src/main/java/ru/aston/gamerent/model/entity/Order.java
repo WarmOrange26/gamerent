ba72bb423d91bb5866988077aadfb95675c63b65
aleksandr_shchennikov_1 <sshennikov@gmail.com>
2023-06-27 10:31:56 +0300

    corrected Order.java & OrdersAccount.java, add changelog id5

--- src/main/java/ru/aston/gamerent/model/entity/Order.java
+++ src/main/java/ru/aston/gamerent/model/entity/Order.java
@@ -3,36 +3,47 @@ package ru.aston.gamerent.model.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.Objects;
+import java.util.ArrayList;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
 @Entity
 @Table(name = "orders")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @ToString.Exclude
     private User user;
 
     @Column(name = "payment_time", nullable = false)
@@ -42,13 +53,14 @@ public class Order {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Order that = (Order) o;
-        return user.equals(that.user)
-                && paymentTime.equals(that.paymentTime);
+
+        return id != null && id.equals(that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(user, paymentTime);
+        return getClass().hashCode();
     }
 }
--- src/main/java/ru/aston/gamerent/model/entity/OrdersAccount.java
+++ src/main/java/ru/aston/gamerent/model/entity/OrdersAccount.java
@@ -13,42 +13,43 @@ import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
 @Entity
 @Table(name = "orders_account")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrdersAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "order_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
     private Order order;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "account_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
     private Account account;
 
-    @Column
+    @Column(nullable = false)
     private int periods;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         OrdersAccount that = (OrdersAccount) o;
-        return order.equals(that.order)
-                && account.equals(that.account)
-                && periods.equals(that.periods);
+
+        return id != null && id.equals(that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(order, account, periods);
+        return getClass().hashCode();
     }
 }
