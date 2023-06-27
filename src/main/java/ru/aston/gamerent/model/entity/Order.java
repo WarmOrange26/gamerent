98a22a4ae91a121c4145f62310f5a4d811ef156b
Aleksandr Shchennikov <122002679+saintsanix@users.noreply.github.com>
2023-06-27 13:03:34 +0300

    Aleksandr shch (#5)
    * add Order.java & OrdersAccount.java
    
    * corrected Order.java & OrdersAccount.java, add changelog id5
    
    * corrected equals&hashcode Order.java & OrdersAccount.java

--- src/main/java/ru/aston/gamerent/model/entity/Order.java
+++ src/main/java/ru/aston/gamerent/model/entity/Order.java
@@ -1,10 +1,64 @@
 package ru.aston.gamerent.model.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+
     @Id
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private User user;
+
+    @Column(name = "payment_time", nullable = false)
+    private LocalDateTime paymentTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order that = (Order) o;
+        return id != null && id.equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
