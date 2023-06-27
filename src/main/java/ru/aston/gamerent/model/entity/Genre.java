dbed21b8b8f511e3dce21b4b577aaa728751b9f8
Masha Chachik <98797561+Twisterly@users.noreply.github.com>
2023-06-27 09:26:15 +0300

    [Gamerent-3] Game, Genre, GenreTitleEnum added, Liquibase changelog file updated (#3)
    * add Genre
    
    * update liquibase-changelog
    
    * add Game
    
    * add GenreTitleEnum
    
    * add imports
    
    * create new chagelog.yaml
    
    * change LocalDate
    
    * add @Builder
    
    * add fetchtype LAZY
    
    * rename changelog
    
    * change Cascade
    
    * add Screenshot to Game
    
    * add comments to @Column
    
    * change Cascade
    
    * add annotation, delete empty line

--- src/main/java/ru/aston/gamerent/model/entity/Genre.java
+++ src/main/java/ru/aston/gamerent/model/entity/Genre.java
@@ -1,10 +1,45 @@
 package ru.aston.gamerent.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.Builder;
 
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
 @Entity
+@Table(name = "genres")
 public class Genre {
     @Id
-    Long id;
+    @Column
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private GenreTitleEnum title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Genre genre)) return false;
+        return title == genre.title;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
 }
---
+++ src/main/java/ru/aston/gamerent/model/entity/GenreTitleEnum.java
@@ -0,0 +1,14 @@
+package ru.aston.gamerent.model.entity;
+
+public enum GenreTitleEnum {
+
+    ACTION_AND_ADVENTURE,
+    DRIVING_AND_RACING,
+    ROLE_PLAYING,
+    SHOOTER,
+    SINGLE_COMBAT,
+    SPORT,
+    STRATEGY,
+    THRILLER
+
+}
