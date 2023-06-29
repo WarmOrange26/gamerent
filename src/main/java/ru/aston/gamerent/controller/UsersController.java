7c4dd234a62b760582cce47395d6e3234f36b551
ValdemarSheesh <107857461+ValdemarSheesh@users.noreply.github.com>
2023-06-29 10:39:23 +0300

    added mapper for User

--- src/main/java/ru/aston/gamerent/controller/UsersController.java
+++ src/main/java/ru/aston/gamerent/controller/UsersController.java
@@ -1,6 +1,7 @@
 package ru.aston.gamerent.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class UsersController {
     private final UsersService usersService;
 
     @GetMapping("/{id}")
-    public String getUser(@PathVariable Integer id) throws NoEntityException {
-        return usersService.getUserById(id);
+    public ResponseEntity<?> getUser(@PathVariable Integer id) throws NoEntityException {
+        return ResponseEntity.ok(usersService.getUserById(id));
     }
 }
--- src/main/java/ru/aston/gamerent/service/UsersService.java
+++ src/main/java/ru/aston/gamerent/service/UsersService.java
@@ -3,6 +3,8 @@ package ru.aston.gamerent.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.aston.gamerent.exception.NoEntityException;
+import ru.aston.gamerent.mapper.UserMapper;
+import ru.aston.gamerent.model.dto.response.UserResponse;
 import ru.aston.gamerent.model.entity.User;
 import ru.aston.gamerent.repository.UsersRepository;
 
@@ -11,9 +13,10 @@ import ru.aston.gamerent.repository.UsersRepository;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final UserMapper userMapper;
 
-    public String getUserById(long id) throws NoEntityException {
-        User user = usersRepository.findById(id).orElseThrow(() -> new NoEntityException("User with id " + id + "was not found"));
-        return user.toString();
+    public UserResponse getUserById(long id) throws NoEntityException {
+        User user = usersRepository.findById(id).orElseThrow(() -> new NoEntityException("User with id " + id + " was not found"));
+        return userMapper.userToUserResponseDto(user);
     }
 }
