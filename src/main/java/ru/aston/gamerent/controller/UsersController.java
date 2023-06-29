f128f5bd92ba54452f1ff38e3a0e365a0420c6f3
ValdemarSheesh <107857461+ValdemarSheesh@users.noreply.github.com>
2023-06-29 18:31:25 +0300

    refactoring

--- src/main/java/ru/aston/gamerent/controller/UsersController.java
+++ src/main/java/ru/aston/gamerent/controller/UsersController.java
@@ -1,12 +1,12 @@
 package ru.aston.gamerent.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.aston.gamerent.exception.NoEntityException;
+import ru.aston.gamerent.model.dto.response.UserResponse;
 import ru.aston.gamerent.service.UsersService;
 
 @RestController
@@ -17,8 +17,8 @@ public class UsersController {
     private final UsersService usersService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUser(@PathVariable Integer id) throws NoEntityException {
-        return ResponseEntity.ok(usersService.getUserById(id));
+    public UserResponse getUser(@PathVariable Integer id) throws NoEntityException {
+        return usersService.getUserById(id);
     }
 
 }
