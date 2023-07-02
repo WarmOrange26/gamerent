f0f00a7dbe19370ecb44d8ddc58755c1616d89ac
Yuri Kaleganov <109655199+Hebsat@users.noreply.github.com>
2023-07-02 07:59:35 +0300

    feat: added blocking accounts by scheduling (#17)
    * feat: added blocking accounts by scheduling
    
    * fix: modify changing password condition
    
    * fix: generated max password length changed to 45 symbols
    
    * fix: stream changed
    
    * fix: PlatformApiConnectionException added
    
    * fix: added saveNewPassword method

---
+++ src/main/java/ru/aston/gamerent/model/exception/PlatformApiConnectionException.java
@@ -0,0 +1,8 @@
+package ru.aston.gamerent.model.exception;
+
+public class PlatformApiConnectionException extends RuntimeException{
+
+    public PlatformApiConnectionException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/aston/gamerent/service/impl/AccountServiceImpl.java
@@ -0,0 +1,58 @@
+package ru.aston.gamerent.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.RandomStringGenerator;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.aston.gamerent.model.entity.Account;
+import ru.aston.gamerent.model.exception.PlatformApiConnectionException;
+import ru.aston.gamerent.repository.AccountRepository;
+import ru.aston.gamerent.service.AccountService;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+    private final Random random = new Random();
+
+    @Override
+    @Scheduled(cron = "${game-rent.scheduling.account-blocking}")
+    @Transactional
+    public void blockAccounts() {
+        accountRepository.findAll().stream()
+                .filter(account -> account.getExpirationTime().isBefore(LocalDateTime.now()) &&
+                        account.getExpirationTime().isAfter(account.getUpdateTime()))
+                .forEach(this::changeAccountPassword);
+    }
+
+    private void changeAccountPassword(Account account) {
+        try {
+            String newPassword = generateNewPassword();
+            //some code that connect to account platform api and set new password
+            log.info("connection to " + account.getPlatform().getName() + " api and set password: " + newPassword);
+            saveNewPassword(account, newPassword);
+        } catch (PlatformApiConnectionException e) {
+            log.warn("connection to " + account.getPlatform().getName() + " was failed");
+        }
+    }
+
+    private String generateNewPassword() {
+        RandomStringGenerator randomStringGenerator = new RandomStringGenerator.Builder()
+                .withinRange(33, 122)
+                .build();
+        return randomStringGenerator.generate(random.nextInt(20) + 25);
+    }
+
+    private void saveNewPassword(Account account, String newPassword) {
+        account.setPassword(newPassword);
+        account.setUpdateTime(LocalDateTime.now());
+        accountRepository.save(account);
+    }
+}
