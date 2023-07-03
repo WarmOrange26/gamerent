b9a7c4075da82e8e375f4fb986cae2e5bb07af6e
Yuri Kaleganov <yurkaleganov@yandex.ru>
2023-07-03 21:02:54 +0300

    fix: added validation to OrderRequest

--- src/main/java/ru/aston/gamerent/model/dto/request/OrderRequest.java
+++ src/main/java/ru/aston/gamerent/model/dto/request/OrderRequest.java
@@ -1,10 +1,20 @@
 package ru.aston.gamerent.model.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.util.List;
 
 public record OrderRequest(
+        @NotNull(message = "Player id is mandatory!")
+        @Positive
         Long playerId,
+        @NotNull(message = "Wallet id is mandatory!")
+        @Positive
         Long walletId,
+        @NotNull(message = "Games ids is mandatory!")
         List<Long> gameIds,
+        @NotNull(message = "Number of periods is mandatory!")
+        @Positive
         Integer periods) {
 }
