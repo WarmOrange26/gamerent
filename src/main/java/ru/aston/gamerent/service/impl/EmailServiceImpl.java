2ad82be78dcc2e9d2eea2486e660b0958588d7ab
Колбаска <aleksandr.mikhalevich89@gmail.com>
2023-07-03 13:15:13 +0300

    [Registration] Mail sending method corrected

--- src/main/java/ru/aston/gamerent/service/impl/EmailServiceImpl.java
+++ src/main/java/ru/aston/gamerent/service/impl/EmailServiceImpl.java
@@ -18,28 +18,37 @@ public class EmailServiceImpl implements EmailService {
     @Value("${spring.mail.username}")
     private String sender;
 
-    @Override
-    public String sendRegistrationMail(String username, String email, String password) {
-        try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom(sender);
-            mailMessage.setTo(email);
-            mailMessage.setText(String.format("""
+    private static final String TEXT_MESSAGE = """
                             Good day!
                             You have successfully registered in WARM online gamerent shop.
                             You username: %s
                             E-mail: %s
                             Password: %s
-                            """, username, email, password)
-                    );
-            mailMessage.setSubject("WARM online gamerent shop registration");
+                            """;
+
+    private static final String SUBJECT_MESSAGE = "WARM online gamerent shop registration";
+
+    private static final String USER_INFO_SUCCESS = "A letter with registration data is sent to the e-mail" +
+            " specified during registration. You can now use these details to log into the site";
+
+    private static final String USER_INFO_ERROR = "Аn error occurred during registration email sending!";
+
+    @Override
+    public String sendRegistrationMail(String username, String email, String password) {
+        String userInfo;
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(email);
+            mailMessage.setText(String.format(TEXT_MESSAGE, username, email, password));
+            mailMessage.setSubject(SUBJECT_MESSAGE);
             javaMailSender.send(mailMessage);
             log.info("A letter with registration data is sent to the e-mail: {}", email);
-            return "A letter with registration data is sent to the e-mail specified during registration." +
-                    " You can now use these details to log into the site";
+            userInfo = USER_INFO_SUCCESS;
         } catch (Exception e) {
             log.warn("Аn error occurred during email sending for e-mail: {} with error message: {}", email, e.getMessage());
-            return "Аn error occurred during email sending!";
+            userInfo = USER_INFO_ERROR;
         }
+        return userInfo;
     }
 }
